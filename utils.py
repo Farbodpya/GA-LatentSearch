7bee627e25abe99beacf2fbc3f10e0e400b7e692
Farbodpya <128262522+Farbodpya@users.noreply.github.com>
2025-04-18 12:26:34 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,10 @@
-import matplotlib.pyplot as plt
+import numpy as np
+
+# Projection matrix
+P = np.random.randn(2, 5000)
+P /= np.linalg.norm(P, axis=1, keepdims=True)
+
+def decode(z): return P.T @ z
+
 
 
